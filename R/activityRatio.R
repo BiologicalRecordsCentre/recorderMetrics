31d439bd257ad1d5838bbbeb569401a20acd50d5
AugustT <tomaug@ceh.ac.uk>
2018-09-10 13:26:54 +0100

    activity ratio updated to only use summer

--- R/activityRatio.R
+++ R/activityRatio.R
@@ -6,6 +6,7 @@
 #' @param data the data.frame of recording information
 #' @param recorder_col the name of the column that contains the recorder names
 #' @param date_col the name of the column that contains the date. This must be formatted as a date
+#' @param summer_days Activity ratio is only calculated for the summmer period is this parameter is provided. This is a three column data.frame: year, Jday_start (numeric Julian day of the first day of summer), Jday_end (numeric Julian day of the last day of summer). These are returned as an attribute from \code{summerData}.
 #'  
 #' @export
 #' 
@@ -15,17 +16,22 @@
 #' # load example data
 #' head(cit_sci_data)
 #' 
+#' # Get the summer period
+#' SD <- summerData(cit_sci_data, date_col = 'date')
+#' 
 #' ar <- activityRatio(recorder_name = 3007,
 #'                     data = cit_sci_data,
 #'                     recorder_col = 'recorder',
-#'                     date_col = 'date')
+#'                     date_col = 'date',
+#'                     summer_days = attr(SD, 'cutoffs'))
 #'                     
 #' # Run the metric for all recorders
 #' ar_all <- lapply(unique(cit_sci_data$recorder),
 #'                  FUN = activityRatio,
 #'                  data = cit_sci_data,
 #'                  recorder_col = 'recorder',
-#'                  date_col = 'date')
+#'                  date_col = 'date',
+#'                  summer_days = attr(SD, 'cutoffs'))
 #'
 #' # summarise as one table
 #' ar_all_sum <- do.call(rbind, ar_all)
@@ -36,7 +42,7 @@
 #' @return A data.frame with four columns
 #' \itemize{
 #'  \item{\code{recorder} - }{The name of the recorder, as given in the recorder_name argument}
-#'  \item{\code{activity_ratio} - }{The proportion of days on which the volunteer was active in relation to the total days he/she remained linked to the project (i.e. \code{active_days} / \code{total_duration})}
+#'  \item{\code{activity_ratio} - }{The proportion of days on which the volunteer was active in relation to the total days he/she remained linked to the project (i.e. \code{active_days} / \code{duration}. Duration is either the number of days elapsed from their first to last record, or if \code{summer_days} is provided it is the the number of days elapsed from their first to last record that fall into the summer periods)}
 #'  \item{\code{total_duration} - }{The total number of days the volunteer was involved in the project, calculated as the number of days from the first record they submitted to the last (inclusive)}
 #'  \item{\code{active_days} - }{The total number of unique dates on which observations were made}
 #' }
@@ -45,7 +51,8 @@ activityRatio <-
 function(recorder_name,
          data,
          recorder_col = 'recorders',
-         date_col = 'date_start'){
+         date_col = 'date_start',
+         summer_days = NULL){
   
   # check date column
   if(!inherits(data[, date_col], 'Date')){
@@ -75,14 +82,47 @@ function(recorder_name,
   
   # Total duration of this recorder
   duration <- as.numeric(first_last[2] - first_last[1]) + 1 
+  summer_duration <- NA
   
-  # calculate ratio
-  activity_ratio <- length(dates)/duration
-  
+  if(is.null(summer_days)){
+    
+    # calculate ratio
+    activity_ratio <- length(dates)/duration
+    
+  } else {
+    
+    # This returns a vector is dates that represent a summer for a given year
+    SDconv <- function(x){
+      d0 <- as.Date(paste(as.character(x[1]), '01', '01', sep = '-'))-1
+      s <- d0 + x[2]
+      e <- d0 + x[3]
+      days <- seq(s, e, by = 'days')
+      return(days)
+    }
+    
+    # Apply the function across all years
+    summer_dates <- apply(X = summer_days,
+                          MARGIN = 1,
+                          FUN = SDconv)
+    
+    # Join the result together
+    summer_dates <- do.call(c, summer_dates)
+    
+    # Compare to a vector of all dates a record we around and sum the intersect
+    # this is the total number of summer days passed from beginning to end of 
+    # the recoder's engagement
+    summer_duration <- sum(summer_dates %in% seq(first_last[1], first_last[2], by = 'days'))
+    
+    # calculate ratio
+    activity_ratio <- length(dates)/summer_duration
+    
+  }
+    
   # return
   return(data.frame(recorder = recorder_name,
                     activity_ratio = activity_ratio,
                     total_duration = duration,
+                    summer_duration = summer_duration,
                     active_days = length(dates)))
   }
 }
